1a7612a2e5f1b6431c52a52649d596655038f296
rasyah <rasyahodhe450@gmail.com>
2023-06-15 22:50:07 +0800

    mengubah warna pada span dibagian headgambar pada file css

--- js/scriptlanding.js
+++ js/scriptlanding.js
@@ -1,13 +1,3 @@
-const container = document.querySelector('body div.container');
-const divcontent = document.querySelector('body div.container div.content');
-const divbaru = document.createElement('div');
-const teksdivbaru = document.createTextNode('INI DIVBARU');
-divbaru.appendChild(teksdivbaru);
-divbaru.style.color = "red";
-// divbaru.style.marginTop = '50px';
-container.appendChild(divbaru);
-// container.insertBefore(divcontent, divbaru);
-
 // Even Click pada gambar maka hilang animasinya
 
 const img = document.querySelector('body .head .headgambar img');
