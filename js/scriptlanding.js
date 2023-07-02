21810a4d8f1f7ad15c9f6313d9bd57dca93beece
rasyah <rasyahodhe450@gmail.com>
2023-07-02 12:04:19 +0800

    menambahkan Code pada landingpage.html dan sedikit pada script landing.js

--- js/scriptlanding.js
+++ js/scriptlanding.js
@@ -5,3 +5,11 @@ const span = document.querySelector('body div.head .headgambar span.warna1');
 img.addEventListener('click', function () {
     span.classList.toggle('warna1');
 });
+
+
+const thome = document.getElementById('home');
+thome.addEventListener('click', function () {
+    const ambilangka = Math.round(Math.random() * 255 + 1);
+    console.log(ambilangka);
+    document.body.style.backgroundColor = 'rgb(100,100,' + ambilangka + ')';
+});
