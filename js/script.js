9421dade171812594eb14167b36baaa434d150d5
rasyah <rasyahodhe450@gmail.com>
2023-07-23 22:15:41 +0800

    menambahkan Element pada file html dan css serta javascript

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ const h1 = document.querySelector(".container .head .navinfo h1");
 img.addEventListener("click", function () {
     h1.style.color = "red";
 });
+
+const lipertama = document.querySelector(".container .head .navinfo ul #konten_home")
+const apertama = document.querySelector(".container .head .navinfo ul li a");
+apertama.addEventListener("click", function () {
+    lipertama.classList.toggle("perbesarspan");
+});
+lipertama.addEventListener("click", function () {
+    lipertama.classList.toggle("perbesarspan");
+})
