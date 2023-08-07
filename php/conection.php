2f09a5a8a4448106167e045fd45b40a7a64cc72a
rasyah <rasyahodhe450@gmail.com>
2023-08-07 21:30:18 +0800

    mengubah sedikit file php

--- php/conection.php
+++ php/conection.php
@@ -1,13 +1,13 @@
-<?php 
-$usernma = "";
+<?php
+$usernma = "root";
 $pass = "";
-$localhost ="";
-$database ="";
+$localhost = "127.0.0.1/localhost";
+$database = "admin";
 
-$con = mysqli_connect($localhost,$usernma,$pass,$database);
+$con = mysqli_connect($localhost, $usernma, $pass, $database);
 
-if (!$con){
-    die("Connection Failed : ". mysqli_connect_error());
+if (!$con) {
+    die("Connection Failed : " . mysqli_connect_error());
 }
-echo("Connection Succes full");
+echo ("Connection Succes full");
 mysqli_close($con);
